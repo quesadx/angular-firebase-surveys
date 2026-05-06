bc2194eb31e617e4fc5b7024690194b164f3c719
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-05-05 23:00:14 -0600

    feat: add new route for creating surveys with lazy loading of the component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ import { LoginComponent } from './auth/login.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: LoginComponent },
+  {
+    path: 'surveys/new',
+    loadComponent: () => import('./surveys/new-survey.component').then((m) => m.NewSurveyComponent)
+  },
   { path: '**', redirectTo: '/auth/login' }
 ];
